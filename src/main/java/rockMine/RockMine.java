640212794dd2d0fa411d714a2e119391e60ac7f4
Hancho1577 <vojougae35@gmail.com>
2021-02-26 11:34:25 +0700

    Reformat and refactor code

--- src/main/java/rockMine/RockMine.java
+++ src/main/java/rockMine/RockMine.java
@@ -1,10 +1,11 @@
 package rockMine;
 
-import java.util.HashMap;
 import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
 
 import cn.nukkit.Player;
 import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockID;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
@@ -16,71 +17,77 @@ import cn.nukkit.level.Location;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.scheduler.AsyncTask;
 
-public class RockMine extends PluginBase implements Listener{
-	private HashMap<Location, Integer> resetQueue = new HashMap<Location, Integer>();
-	private Level caveLevel;
-	private Block stoneBlock;
-	
-	@Override
-	public void onEnable() {
-		if(getServer().loadLevel("cave")) {
-			this.caveLevel = this.getServer().getLevelByName("cave");
-		}else {
-			getLogger().info("Could not find level 'cave'.");
-			getServer().getPluginManager().disablePlugin(this);
-		}
-		this.stoneBlock = Block.get(Block.STONE);
-		this.getServer().getScheduler().scheduleRepeatingTask(this, new AsyncTask() {
-			
-			@Override
-			public void onRun() {
-				resetMine(false);
-			}
-			
-		},30,true);
-		this.getServer().getPluginManager().registerEvents(this, this);
-	}
-	
-	@Override
-	public void onDisable() {
-		resetMine(true);
-	}
-	
+public class RockMine extends PluginBase implements Listener {
+    private ConcurrentHashMap<Location, Long> resetQueue = new ConcurrentHashMap<>();
+    private Level caveLevel;
+    private Block stoneBlock;
+
+    @Override
+    public void onEnable() {
+        if (getServer().loadLevel("cave")) {
+            this.caveLevel = this.getServer().getLevelByName("cave");
+        } else {
+            getLogger().info("Could not find level 'cave'.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        this.stoneBlock = Block.get(Block.STONE);
+        this.getServer().getScheduler().scheduleRepeatingTask(this, new AsyncTask() {
+
+            @Override
+            public void onRun() {
+                resetMine(false);
+            }
+
+        }, 30, true);
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        resetMine(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBreak(BlockBreakEvent ev) {
+        Player player = ev.getPlayer();
+        Level level = player.getLevel();
+        Block block = ev.getBlock();
+
+        if (level == this.caveLevel && block.getId() == BlockID.STONE) {
+            ev.setCancelled(false);
+            this.resetQueue.put(block.getLocation(), System.currentTimeMillis() / 1000);
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent ev) {
+        if (!(ev.getEntity() instanceof Player)) return;
+        if (ev.getEntity().getLevel() != this.caveLevel) return;
+        if (ev.getCause().equals(DamageCause.SUFFOCATION)) ev.setCancelled(true);
+    }
+
 	public void resetMine(boolean resetAll) {
-		int date = (int) (System.currentTimeMillis() / 1000);
-		if(resetAll) {
-			for(Location location : resetQueue.keySet()) {
-				this.caveLevel.setBlock(location, this.stoneBlock);
+		long current = System.currentTimeMillis() / 1000;
+
+		if (resetAll) {
+			for (Location location : resetQueue.keySet()) {
+				this.caveLevel.setBlock(location, this.stoneBlock, false, false);
 			}
-			this.resetQueue = new HashMap<Location, Integer>();
-			return;
+
+			this.resetQueue.clear();
 		} else {
 			Iterator<Location> it = this.resetQueue.keySet().iterator();
-			while(it.hasNext()) {
+
+			while (it.hasNext()) {
 				Location location = it.next();
-				if((date - this.resetQueue.get(location)) < 30) continue;
-				this.caveLevel.setBlock(location, this.stoneBlock);
+				if ((current - this.resetQueue.get(location)) < 30) {
+					continue;
+				}
+
+				this.caveLevel.setBlock(location, this.stoneBlock, false, false);
 				it.remove();
 			}
 		}
 	}
-	
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onBreak(BlockBreakEvent ev) {
-		if(ev.getBlock().getId() != Block.STONE) return;
-		Player player = ev.getPlayer();
-		Level level = player.getLevel();
-		Block block = ev.getBlock();
-		if(level != this.caveLevel) return;
-		if(ev.isCancelled()) ev.setCancelled(false);
-		this.resetQueue.put(block.getLocation(), (int) (System.currentTimeMillis() / 1000));
-	}
-	
-	@EventHandler
-	public void onDamage(EntityDamageEvent ev) {
-		if(!(ev.getEntity() instanceof Player)) return;
-		if(ev.getEntity().getLevel() != this.caveLevel) return;
-		if(ev.getCause().equals(DamageCause.SUFFOCATION)) ev.setCancelled(true);
-	}
-
 }
